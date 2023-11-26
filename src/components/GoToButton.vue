9f33c3324d15d4994b0bd3bbeb4839a7b6899086
IanFan <fancyian05180910@gmail.com>
2023-11-26 09:46:37 +0800

    製作按鈕元件

---
+++ src/components/GoToButton.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="goto">
+      <div class="d-flex align-items-center">
+        {{ value }}<div class="ms-2 right_icon"></div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/main.scss';
+.right_icon {
+  width: 32px;
+  height: 32px;
+  background-image: url('@/assets/image/arrow-right-short.svg');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.goto {
+  padding: 16px 24px;
+  border-radius: 500px;
+  background-color: $gray-100;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 19px;
+  letter-spacing: 0em;
+  text-align: left;
+  display: inline-block;
+  cursor: pointer;
+}
+</style>

fb39d0b7edaae74193890c741ddaaaba77712469
EPAY-PRESCO\ian.fan <ian.fan@presco.ws>
2023-12-05 15:24:18 +0800

    設定頁面Title

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production' ? '/2023_F2E_Week1/' : '/',
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: config => {
+    config.plugin('html')
+      .tap(args => {
+        args[0].title = '六角學院F2E - 立委競選官網'
+        return args
+      })
+  }
 })

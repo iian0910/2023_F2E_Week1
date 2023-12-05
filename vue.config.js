5e37a6f27ba6fadc1eff19c66be4269a63425109
EPAY-PRESCO\ian.fan <ian.fan@presco.ws>
2023-12-05 14:58:13 +0800

    新增 deploy 編譯檔 / 調整 config 對應路徑

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/2023_F2E_Week1/' : '/',
   transpileDependencies: true
 })
